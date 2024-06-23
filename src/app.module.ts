c849f456a98c55d504d47e5b57f9c948124ab43f
eric <eric@simplytechnologies.net>
2024-06-23 16:24:02 +0400

    feat: add global error handling middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
+
+import { validate } from './env.validation';
+import { ProductModule } from './resources/product/product.module';
+import { CategoryModule } from './resources/category/category.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      validate,
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    ProductModule,
+    CategoryModule,
+  ],
+  providers: [],
 })
 export class AppModule {}
